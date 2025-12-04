3c4ef2a47361aa222140aadc39cf4f0bc6afc9c9
MattNotarangelo <matt.notarangelo@rokt.com>
2025-12-04 10:32:59 -0800

    day 4

---
+++ 04/part_2.py
@@ -0,0 +1,49 @@
+# https://adventofcode.com/2025/day/04
+
+PAPER = "@"
+NO_PAPER = "."
+
+
+class Solution:
+    def __init__(self):
+        self.data = []
+
+    def _parse_input(self, s: str):
+        rows = s.strip().split("\n")
+        self.data = [list(i) for i in rows]
+
+    def _contains_paper(self, x, y):
+        if x < 0 or y < 0 or x >= len(self.data[0]) or y >= len(self.data):
+            return False
+
+        return self.data[y][x] == PAPER
+
+    def _check_position(self, x, y):
+
+        adjacent = (
+            self._contains_paper(x + 1, y)
+            + self._contains_paper(x + 1, y + 1)
+            + self._contains_paper(x, y + 1)
+            + self._contains_paper(x - 1, y + 1)
+            + self._contains_paper(x - 1, y)
+            + self._contains_paper(x - 1, y - 1)
+            + self._contains_paper(x, y - 1)
+            + self._contains_paper(x + 1, y - 1)
+        )
+
+        return adjacent < 4
+
+    def solve(self, s: str):
+        changed = True
+        removed = 0
+        self._parse_input(s)
+        while changed == True:
+            changed = False
+            for y in range(len(self.data)):
+                for x in range(len(self.data[y])):
+                    if self.data[y][x] == PAPER:
+                        if self._check_position(x, y):
+                            self.data[y][x] = NO_PAPER
+                            changed = True
+                            removed += 1
+        return removed
